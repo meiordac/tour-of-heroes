1970ed6af2a086ab627b14995d820d939d3de8ef
meiordac <matias.iordache@gmail.com>
2016-05-20 19:15:58 -0400

    created css styles for components

--- app/heroes.component.js
+++ app/heroes.component.js
@@ -9,12 +9,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var hero_detail_component_1 = require('./hero-detail.component');
 var hero_service_1 = require('./hero.service');
+var router_deprecated_1 = require('@angular/router-deprecated');
 var HeroesComponent = (function () {
-    function HeroesComponent(heroService) {
+    function HeroesComponent(router, heroService) {
+        this.router = router;
         this.heroService = heroService;
-        this.title = 'Tour of Heroes';
     }
     HeroesComponent.prototype.getHeroes = function () {
         var _this = this;
@@ -24,15 +24,16 @@ var HeroesComponent = (function () {
         this.getHeroes();
     };
     HeroesComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+    HeroesComponent.prototype.gotoDetail = function () {
+        this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    };
     HeroesComponent = __decorate([
         core_1.Component({
             selector: 'my-heroes',
             templateUrl: 'app/heroes.component.html',
-            styleUrls: ['app/heroes.component.css'],
-            directives: [hero_detail_component_1.HeroDetailComponent],
-            providers: [hero_service_1.HeroService]
+            styleUrls: ['app/heroes.component.css']
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [router_deprecated_1.Router, hero_service_1.HeroService])
     ], HeroesComponent);
     return HeroesComponent;
 }());
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
+import { RouteParams } from '@angular/router-deprecated';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router-deprecated';
+
+
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls:  ['app/heroes.component.css'],
-  directives: [HeroDetailComponent],
-  providers: [HeroService]
+  styleUrls:  ['app/heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
@@ -21,4 +24,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   onSelect(hero: Hero) { this.selectedHero = hero; }
+  gotoDetail() {
+    this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
 }
