4e51eb76c7c7e3e8d55e0693ca32aac7f5e9b812
meiordac <matias.iordache@gmail.com>
2016-05-29 16:58:15 -0400

    modified hero.service in order to create, delete and modify heroes with http

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
 var angular2_in_memory_web_api_1 = require('angular2-in-memory-web-api');
 var in_memory_data_service_1 = require('./in-memory-data.service');
+// The usual bootstrapping imports
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var http_2 = require('@angular/http');
 var app_component_1 = require('./app.component');
